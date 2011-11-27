5c44991fdeaad6f66f288ef4b184d6bf9ff34ee3
kevinhascode <kevindoesstuff@gmail.com>
2011-11-26 19:19:19 -0500

    creation

---
+++ GoGame/GoGame.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace GoGame
+{
+    partial class GoGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnPass = new System.Windows.Forms.Button();
+            this.btnEditContinue = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).BeginInit();
+            this.mainSplitContainer.Panel2.SuspendLayout();
+            this.mainSplitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainSplitContainer
+            // 
+            this.mainSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.mainSplitContainer.Name = "mainSplitContainer";
+            // 
+            // mainSplitContainer.Panel2
+            // 
+            this.mainSplitContainer.Panel2.Controls.Add(this.btnEditContinue);
+            this.mainSplitContainer.Panel2.Controls.Add(this.btnPass);
+            this.mainSplitContainer.Panel2.Controls.Add(this.btnNewGame);
+            this.mainSplitContainer.Size = new System.Drawing.Size(832, 466);
+            this.mainSplitContainer.SplitterDistance = 608;
+            this.mainSplitContainer.TabIndex = 0;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(65, 158);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(75, 23);
+            this.btnNewGame.TabIndex = 0;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            // 
+            // btnPass
+            // 
+            this.btnPass.Location = new System.Drawing.Point(65, 129);
+            this.btnPass.Name = "btnPass";
+            this.btnPass.Size = new System.Drawing.Size(75, 23);
+            this.btnPass.TabIndex = 0;
+            this.btnPass.Text = "Pass";
+            this.btnPass.UseVisualStyleBackColor = true;
+            // 
+            // btnEditContinue
+            // 
+            this.btnEditContinue.Location = new System.Drawing.Point(65, 187);
+            this.btnEditContinue.Name = "btnEditContinue";
+            this.btnEditContinue.Size = new System.Drawing.Size(75, 42);
+            this.btnEditContinue.TabIndex = 0;
+            this.btnEditContinue.Text = "Edit / Continue";
+            this.btnEditContinue.UseVisualStyleBackColor = true;
+            // 
+            // GoGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 466);
+            this.Controls.Add(this.mainSplitContainer);
+            this.Name = "GoGame";
+            this.Text = "GoGame";
+            this.Load += new System.EventHandler(this.GoGame_Load);
+            this.mainSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplitContainer)).EndInit();
+            this.mainSplitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer mainSplitContainer;
+        private System.Windows.Forms.Button btnEditContinue;
+        private System.Windows.Forms.Button btnPass;
+        private System.Windows.Forms.Button btnNewGame;
+    }
+}
---
+++ GoGame/GoGame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GoGame
+{
+    public partial class GoGame : Form
+    {
+        private int n;
+
+        public GoGame()
+        {
+            InitializeComponent();
+            this.n = 9;
+        }
+
+        private void GoGame_Load(object sender, EventArgs e)
+        {
+            this.paintSomeShit();
+        }
+
+        private void paintSomeShit()
+        {
+            SplitterPanel gamePanel = mainSplitContainer.Panel1;
+            int H = gamePanel.Height;
+            int W = gamePanel.Width;
+
+            int totalLength = (W <= H) ? W : H;
+            int a = totalLength / n;
+
+            int D = Math.Abs(W - H);
+
+            //for (int i = 0; i < n - 1; ++i)
+            using (Graphics g = gamePanel.CreateGraphics())
+            {
+                using (Pen pen = new Pen(Color.Black, 2))
+                {
+                    using (Brush brush = new SolidBrush(gamePanel.BackColor))
+                    {
+                        //System.Drawing.Rectangle rect = new Rectangle(new Point(a, a), new Size(a, a));
+                        g.DrawRectangle(pen, 100, 100, 100, 200);
+                    }
+                }
+            }
+        }
+    }
+}
