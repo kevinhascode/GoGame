17153b438ef94e05df372d3b0411aba12d610939
kevinhascode <kevindoesstuff@gmail.com>
2011-11-27 10:44:36 -0500

    adding Loc, Stone

---
+++ GoGame/Loc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoGame
+{
+    internal class Loc
+    {
+        internal int X { get; set; }
+        internal int Y { get; set; }
+
+        public Loc(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ GoGame/Stone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoGame
+{
+    internal class Stone
+    {
+        internal Loc Loc { get; set; }
+        internal bool IsWhite { get; set; }
+
+        public Stone(Loc loc , bool isWhite)
+        {
+            Loc = loc;
+            IsWhite = isWhite;
+        }
+    }
+}
